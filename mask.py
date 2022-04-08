83810973a98197ebeab610c1905a45940db97d12
Tech Penguin <85949246+TechPenguineer@users.noreply.github.com>
2022-04-08 16:59:42 -0400

    facial detection added

--- mask.py
+++ mask.py
@@ -2,4 +2,8 @@ import cv2
 import numpy as np 
 
 def getColorMask(img):
-    lowerBound = np.array([])
+    lowerBound = np.array([0,180,255])
+    upperBound = np.array([255,255,255])
+    
+    hsv = cv2.cvtColor(img, cv2.COLOR_BGR2HSV)
+    return cv2.inRange(hsv, lowerBound, upperBound)
